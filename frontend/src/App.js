d557bc6b9108154d74dc92e33f45a4baebb83dcf
koyna khare <92796533+koynakhare@users.noreply.github.com>
2023-07-01 17:33:08 +0530

    hi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -54,7 +54,7 @@ const App = () => {
 
   return (
     <>
-      <h1>hi</h1>
+      <h1>hi koyna</h1>
       <Routes>
         <Route exact={true} path={`/`} element={<Main />}></Route>
         <Route
